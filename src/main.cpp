9a96607f7bff51baae9685cc573138d0af794584
Ilyana Megy <ilymegy@bess-f3r5s2.clusters.42paris.fr>
2025-02-17 17:02:32 +0100

    :test_tube: [TESTING] multiple servers in config file tests OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,53 @@
 #include "../inc/Network/Epoll.hpp"
 #include "../inc/Network/ServerMonitor.hpp"
 
-int runServer(std::vector<ServerConf> serversConfig) {
-	ServerMonitor servers;
-
-	servers.createServers(serversConfig);
-	std::cout << PINK << "Server created" << RESET << std::endl;
+// int runServer(std::vector<ServerConf> serversConfig) {
+	
+// 	std::cout << PINK << "Server created" << RESET << std::endl;
 
 
-	// Epoll epoll(server.getSocket().getFd());
+// 	// Epoll epoll(server.getSocket().getFd());
 
-	// while (true) {
-	// 	try {
-	// 		epoll.wait();
-	// 	} catch (std::exception& e) {
-	// 		std::cout << e.what() << std::endl;
-	// 	}
+// 	// while (true) {
+// 	// 	try {
+// 	// 		epoll.wait();
+// 	// 	} catch (std::exception& e) {
+// 	// 		std::cout << e.what() << std::endl;
+// 	// 	}
 
-	// 	// for (int i = 0; i < epoll.getReadyFd(); i++) {
-	// 	// 	if (epoll.getFd(i) == server.getSocket().getFd()) {
-	// 	// 		try {
-	// 	// 			server.acceptClient();
-	// 	// 		} catch (std::exception& e) {
-	// 	// 			std::cout << e.what() << std::endl;
-	// 	// 		}
-	// 	// 	} else {
-	// 	// 		server.readFrom(i);
-	// 	// 		server.sendTo(i);
-	// 	// 	}
-	// 	// }
-	// }
-	return 0;
-}
+// 	// 	// for (int i = 0; i < epoll.getReadyFd(); i++) {
+// 	// 	// 	if (epoll.getFd(i) == server.getSocket().getFd()) {
+// 	// 	// 		try {
+// 	// 	// 			server.acceptClient();
+// 	// 	// 		} catch (std::exception& e) {
+// 	// 	// 			std::cout << e.what() << std::endl;
+// 	// 	// 		}
+// 	// 	// 	} else {
+// 	// 	// 		server.readFrom(i);
+// 	// 	// 		server.sendTo(i);
+// 	// 	// 	}
+// 	// 	// }
+// 	// }
+// 	return 0;
+// }
 
 int main(int ac, char** av) {
 	if (ac != 1 && ac != 2)
 		return (std::cout << "Error: wrong number of arguments." << std::endl, 1);
 
-	// ! \ change name ConfigParser to "ServersLst" be-like
 	ConfigParser data;
+	ServerMonitor servers;
 	try {
 		data.createCluster(ac == 1 ? "config/webserv.conf" : av[1]);
 
 	} catch (std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		return 1;
+		return (std::cout << e.what() << std::endl, 1);
 	}
 
-
 	try {
-		runServer(data.getServers());
+		servers.setupServers(data.getServers());
 	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-		return 1;
+		return (std::cout << e.what() << std::endl, 1);
 	}
 	return 0;
 }
