b32c9cd85716c2aa3e055be9047c181721c93e38
Ilyana Megy <ilymegy@bess-f3r4s2.clusters.42paris.fr>
2025-03-31 16:15:14 +0200

    :heavy_minus_sign: [RM] useless files

--- www/webserv/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var header = document.getElementById("username");
-    if (userName && userName != "") {
-        header.textContent = userName;
-    } else {
-        header.textContent = "Stranger";
-    }
-    document.getElementById("greetings").classList.add("animate-grow");
-});
